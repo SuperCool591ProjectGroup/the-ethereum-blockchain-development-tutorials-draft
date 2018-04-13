95978836a23baf189a979c8aa89c3b481a586142
Jungho Bang <me@bangtoven.com>
2018-04-13 15:23:10 -0400

    Yijia's cool work!

--- 02-Testing/finish/contracts/HelloEthSalon.sol
+++ 02-Testing/finish/contracts/HelloEthSalon.sol
@@ -2,6 +2,43 @@ pragma solidity ^0.4.4;
 
 contract HelloEthSalon {
   string message = "I know smart contract testing!!";
+  string[] public songs;
+  address[] public users;
+  // assume one song only has one copyright holder for now
+  mapping(string => ShareHolder) holderInfo;
+  mapping(string => uint) priceInfo;
+  mapping(string => address[]) authorization;
+
+  struct ShareHolder {
+    address add;
+    uint share;
+  }
+
+  // function userRegister() returns (address[]){
+  function userRegister() returns (string){
+    users.push(msg.sender);
+    return "user registered";
+    // return users;
+  }
+
+  function registerCopyright(string song, uint price) public returns (string) {
+    require(checkUserExists(msg.sender));
+    songs.push(song);
+    priceInfo[song] = price;
+    holderInfo[song].add = msg.sender;
+    holderInfo[song].share = 1;
+    // authorization[song] = address[];
+    // return holderInfo[song].add;
+    return "copyright registered";
+  }
+
+  function checkUserExists(address user) public constant returns(bool) {
+    for(uint i = 0; i < users.length; i++) {
+      if(users[i] == user)
+        return true;
+    }
+    return false;
+  }
   
   function HelloEthSalon() {
     // constructor
--- 02-Testing/finish/test/hello_eth_salon.js
+++ 02-Testing/finish/test/hello_eth_salon.js
@@ -6,4 +6,19 @@ contract("HelloEthSalon:GetMessage", function (accounts) {
     const result = await contract.GetMessage.call();
     assert.isTrue(result === "I know smart contract testing!!");
   });
+
+  it("should return registered", async function () {
+    const contract = await HelloEthSalon.deployed();
+    const result = await contract.userRegister.call();
+    // console.log(result);
+    assert.isTrue(result === "user registered");
+  });
+
+  it("should return Copyright registered", async function () {
+    const contract = await HelloEthSalon.deployed();
+    const result = await contract.registerCopyright.call("hello world", 1000);
+    console.log(result);
+    assert.isTrue(result === "True");
+  });
+
 });
